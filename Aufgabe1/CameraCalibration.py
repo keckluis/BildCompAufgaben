87e479bcb69e6a0978204268d9c6351d67f6dbf9
Luis Keck <keckluis@gmail.com>
2022-03-28 20:14:04 +0200

    Aufgabe 1 first draft

--- Aufgabe1/CameraCalibration.py
+++ Aufgabe1/CameraCalibration.py
@@ -1,50 +1,73 @@
-from cgi import test
 import numpy as np
 import cv2 as cv
+import glob
 
 cap = cv.VideoCapture(0)
-width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
-height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
-print('width: ' + str(width) + ', height: ' + str(height))
 
 cv.namedWindow('webcam',  cv.WINDOW_FREERATIO)
-print('Press Q to close the window.')
+    
+#read all images in folder
+file_names = [img for img in glob.glob('Aufgabe1/CalibrationPics/*.png')]
 
-def showLiveImage():
+cb_pics_count = len(file_names)
+if  cb_pics_count > 0:
+    print('Found ' + str(cb_pics_count) + ' calibration pictures.')
+    if cb_pics_count < 10:
+        print('WARNING: At least 10 pictures are recommended for camera calibration.')
+    print('Press Q to close the window.')   
+else:
+    print("No calibration pictures found. Please capture them with 'CaptureCalibrationPics.py' first.")
+    cap.release()
+    cv.destroyAllWindows()
+    quit()
+
+calibration_pics = []
+for img in file_names:
+    cb_pic = cv.imread(img)
+    cb_pic = cv.cvtColor(cb_pic, cv.COLOR_BGR2GRAY)
+    calibration_pics.append(cb_pic)
+
+#calculate distortion
+objpoints = []
+imgpoints = []
+
+objp = np.zeros((6*7,3), np.float32)
+objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+for cb_pic in calibration_pics:
+    ret, corners = cv.findChessboardCorners(cb_pic, (7, 6), None)
+    if ret:
+        objpoints.append(objp)
+        imgpoints.append(corners)
+
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, calibration_pics[0].shape[::-1], None, None)
+
+h, w = calibration_pics[0].shape[:2]
+new_camera_matrix, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+
+#remove distortion of current frame
+def removeDistortion(frame):
+    frame = cv.undistort(frame, mtx, dist, None, new_camera_matrix)
+    x, y, w, h = roi
+    frame = frame[y:y+h, x:x+w]
+    return frame
+      
+#video loop
+while True:
+    #quit
+    if cv.waitKey(10) == ord('q'):
+        print('Quit.')
+        break
+    
+    #live image
     ret, frame = cap.read()
     if ret:
+        frame = removeDistortion(frame)
         cv.imshow('webcam', frame)
-        return frame
     else:
         print('Camera not found.')
 
-def captureFrame():
-    while True:
-        ret, current_frame = cap.read()
-        if ret:
-            print('Test frame captured.')
-            return current_frame    
-
-test_frames = []
-while True:
-    if cv.waitKey(10) == ord('q'):
-        break
-    if cv.waitKey(10) == ord('c'):
-        test_frames.append(captureFrame())
-    if cv.waitKey(10) == ord('s'):
-        i = 0
-        for ts in test_frames:
-            cv.imshow('test frame ' + str(i), ts)
-            i = i+1
-    showLiveImage()
-
 cap.release()
 cv.destroyAllWindows()
-
-#TODO: read at least 10 frames with chessboard image (live or prepared?)
-
-#TODO: calculate distortion of camera based on chessboard images
-
-#TODO: remove distortion from each frame in live video feed
-
-#TODO: compute reprojection error for one image (mm)
