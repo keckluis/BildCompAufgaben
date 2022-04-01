50de672beb8f18a6bc61eb00e09cb29941cff657
Luis Keck <keckluis@gmail.com>
2022-04-01 16:16:14 +0200

    code cleanup

--- Aufgabe1/CameraCalibration.py
+++ Aufgabe1/CameraCalibration.py
@@ -1,23 +1,22 @@
 import numpy as np
 import cv2 as cv
 
-#code based on OpenCV documentation: https://docs.opencv.org/4.5.5/dc/dbb/tutorial_py_calibration.html
-#code from documentation is marked with 'OpenCV'
+# code based on OpenCV documentation: https://docs.opencv.org/4.5.5/dc/dbb/tutorial_py_calibration.html
+# code from documentation is marked with 'OpenCV'
 
+# user instructions
 print('Press Q to quit.')
 
 cap = cv.VideoCapture(0)
 
-#window names
+# window names
 window_original = 'webcam original'
 window_undistorted = 'webcam undistorted'
 
-criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-
-#get file with calibration data
+# get file with calibration data
 file_name = input('Enter the name of your calibration file (without format) >')
 
-#try to load file and ask for new input if not found
+# try to load file and ask for new input if not found
 while True:
     try:
         calibration_data = np.load('Aufgabe1/' + file_name + '.npz')
@@ -28,7 +27,7 @@ while True:
 obj_points = calibration_data['obj_points']
 img_points = calibration_data['img_points']
 
-#quit if file is not usable
+# quit if file is not usable
 cal_images = len(obj_points)
 if cal_images == 0:
     print('Invalid calibration data. Try a different file.')
@@ -38,40 +37,45 @@ if cal_images == 0:
 else:
     print('Found data of ' + str(cal_images) + ' calibration images.')
 
-ref_img = cv.imread('Aufgabe1/ref_img.png')
-ref_img = cv.cvtColor(ref_img, cv.COLOR_BGR2GRAY)
+# get camera frame for size reference
+ret, ref_frame = cap.read()
+if ret:
+    ref_img = cv.cvtColor(ref_frame, cv.COLOR_BGR2GRAY)
+else:
+    print('Camera not found.')
+    quit()
 
-#OpenCV: calibrate camera based on calibration data
+# OpenCV: calibrate camera based on calibration data
 ret, intrinsic_mtx, dist_coeffs, rotation_vecs, translation_vecs = cv.calibrateCamera(obj_points, img_points, ref_img.shape[::-1], None, None)
 
-#OpenCV: calculate new camera matrix to remove distortion of a frame
+# OpenCV: calculate new camera matrix to remove distortion of a frame
 h, w = ref_img.shape[:2]
 new_camera_matrix, roi = cv.getOptimalNewCameraMatrix(intrinsic_mtx, dist_coeffs, (w, h), 1, (w, h))
 
-#OpenCV: remove distortion of current frame
+# OpenCV: remove distortion of current frame
 def removeDistortion(frame):
-    frame = cv.undistort(frame, intrinsic_mtx, dist_coeffs, None, new_camera_matrix)
+    frame_undistorted = cv.undistort(frame, intrinsic_mtx, dist_coeffs, None, new_camera_matrix)
     x, y, w, h = roi
-    frame = frame[y:y+h, x:x+w]
-    return frame
+    frame_undistorted = frame_undistorted[y:y+h, x:x+w]
+    return frame_undistorted
 
-#two windows for comparison
-cv.namedWindow(window_original,  cv.WINDOW_FREERATIO)
-cv.namedWindow(window_undistorted,  cv.WINDOW_FREERATIO)
+# two windows for comparison
+cv.namedWindow(window_original, cv.WINDOW_FREERATIO)
+cv.namedWindow(window_undistorted, cv.WINDOW_FREERATIO)
 
-#video loop
+# video loop
 while True:
     #quit
     if cv.waitKey(10) == ord('q'):
         print('Quit.')
         break
     
-    #display live image
+    # display live image
     ret, frame = cap.read()
     if ret:
         frame_undistorted = removeDistortion(frame)
 
-        #show original and undistorted image for comparison
+        # show original and undistorted image for comparison
         cv.imshow(window_original, frame)
         cv.imshow(window_undistorted, frame_undistorted)
     else:
--- Aufgabe1/CaptureCalibrationImages.py
+++ Aufgabe1/CaptureCalibrationImages.py
@@ -2,10 +2,10 @@ import cv2 as cv
 import numpy as np
 import time
 
-#code based on OpenCV documentation: https://docs.opencv.org/4.5.5/dc/dbb/tutorial_py_calibration.html
-#code from documentation is marked with 'OpenCV'
+# code based on OpenCV documentation: https://docs.opencv.org/4.5.5/dc/dbb/tutorial_py_calibration.html
+# code from documentation is marked with 'OpenCV'
 
-#user instructions
+# user instructions
 print('Press Q to quit.')
 print('Press C to start/stop capturing mode.')
 print('Press E to evaluate captured images.')
@@ -13,51 +13,57 @@ print('Data confirmed by evaluation will be saved on quit.')
 
 cap = cv.VideoCapture(0)
 
-#window names
+# window names
 window_calibration = 'calibration picture'
 window_webcam = 'webcam'
 
 cv.namedWindow(window_webcam,  cv.WINDOW_FREERATIO)
 
-#OpenCV: needed for displaying corners in calibration images
+# OpenCV: needed for displaying corners in calibration images
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
+# to save captured and 'chosen for calibration' images
 captured_images = []
 calibration_images = []
 
+# saves one image per second for capturing calibration images
 def captureMode(captured_images):
     while True:
         ret, frame = cap.read()
         if ret:
+            # save current frame
             captured_images.append(frame)
             cv.imshow(window_webcam, frame)
-
+    	
+        # quit capture mode
         if cv.waitKey(10) == ord('c'):
             print('Capturing mode stopped.')
             return 
         
-        #reduced frame rate to allow repositioning of chessboard
+        # reduced frame rate to allow repositioning of chessboard
         time.sleep(1)
 
-#finds usable calibration images and displays them
-def evaluateImages(calibration_images, captured_images):    
+# finds usable calibration images and displays them 
+# user can choose to save or delete individual images
+def evaluateImages(calibration_images, captured_images):  
+    # user instructions  
     print('Evaluating captured images...')
     print('S: save image, D: delete image')
 
     cv.namedWindow(window_calibration, cv.WINDOW_FREERATIO)
 
     for img in captured_images:
-        #OpenCV: look for chessboard in image
+        # OpenCV: look for chessboard in image
         img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
         found_cb, corners = cv.findChessboardCorners(img_gray, (7,6), None)
 
         if found_cb:
-            #OpenCV: show corners for manual verification
+            # OpenCV: show corners for manual verification
             corners2 = cv.cornerSubPix(img_gray, corners, (11,11), (-1,-1), criteria)
             cv.drawChessboardCorners(img, (7,6), corners2, ret)
             cv.imshow('calibration picture', img)
 
-            #save confirmed calibration image to separate array
+            # save confirmed calibration image to separate array
             while True:
                 key_press = cv.waitKey(10)
                 if key_press == ord('s'):
@@ -65,14 +71,18 @@ def evaluateImages(calibration_images, captured_images):
                     break
                 elif key_press == ord('d'):
                     break
+
     print('Saved ' + str(len(calibration_images)) + ' calibration images.')
-    captured_images = [] #empty array to avoid multiple evaluations of one image
+
+    # empty array to avoid multiple evaluations of one image
+
+    captured_images = []
     cv.destroyWindow(window_calibration)
     return calibration_images, captured_images
 
-#save calibration data as arrays
+# save calibration data as arrays
 def saveCalibrationData(calibration_images):
-    #OpenCV: calculate calibration data based on chosen images
+    # OpenCV: calculate calibration data based on chosen images
     obj_points = []
     img_points = []
 
@@ -85,32 +95,30 @@ def saveCalibrationData(calibration_images):
             obj_points.append(obj_p)
             img_points.append(corners)
     
-    #saves calibration data in file named by user input in console
-    file_name = input('Enter a file name for your calibration data (without format)>')
+    # saves calibration data in file named by user input in console
+    file_name = input('Enter a file name for your calibration data (without format) >')
     np.savez('Aufgabe1/' + file_name + '.npz', obj_points=obj_points, img_points=img_points)
-    if len(calibration_images) > 0: 
-        cv.imwrite('Aufgabe1/ref_img.png', calibration_images[0])
     
-#video loop
+# video loop
 while True:
     key_press = cv.waitKey(10)
     
-    #capture calibration images
+    # capture calibration images
     if key_press == ord('c'):
         print('Capturing calibration image...')
         captureMode(captured_images)
 
-    #find usable calibration images in captured collection
+    # find usable calibration images in captured collection
     elif key_press == ord('e'):
         calibration_images, captured_images = evaluateImages(calibration_images, captured_images)
     
-    #quit
+    # quit
     elif key_press == ord('q'):
         saveCalibrationData(calibration_images)
         print('Quit.')
         break
 
-    #display live image
+    # display live image
     ret, frame = cap.read()
     if ret:
         cv.imshow(window_webcam, frame)
