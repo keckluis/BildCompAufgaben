0804bff075878f12c8133b5d392e993cec3a7ce7
Luis Keck <keckluis@gmail.com>
2022-04-13 15:26:34 +0200

    height measurement complete

--- Aufgabe2/HeightMeasurement.py
+++ Aufgabe2/HeightMeasurement.py
@@ -5,7 +5,7 @@ window_name = 'window'
 window = cv2.namedWindow(window_name, cv2.WINDOW_FREERATIO)
 
 img = cv2.imread('Aufgabe2/table_bottle_01.jpg', cv2.IMREAD_COLOR)
-orig_img = img.copy()
+cv2.imshow(window_name, img)
 height, width, _ = img.shape
 
 def getIntersectionPosition(line1, line2):
@@ -19,12 +19,14 @@ def getLine(point1, point2):
     line = np.cross(point1, point2)
     return line
 
-def getCloserPoint(intersec_pos, point1, point2):
-    intersec_pos = np.array((intersec_pos[0], intersec_pos[1]))
-    p1 = np.array((point1[0], point1[1]))
-    p2 = np.array((point2[0], point2[1]))
-    dist1 = np.linalg.norm(p1 - intersec_pos)
-    dist2 = np.linalg.norm(p2 - intersec_pos)
+def getDistance(point1, point2):
+    point1 = np.array((point1[0], point1[1]))
+    point2 = np.array((point2[0], point2[1]))
+    return np.linalg.norm(point1 - point2)
+
+def getCloserPoint(intersec_pos, point1, point2): 
+    dist1 = getDistance(point1, intersec_pos)
+    dist2 = getDistance(point2, intersec_pos)
     if dist1 <= dist2:
         return point1
     else:
@@ -46,7 +48,7 @@ def click(event, x, y, flags, param):
         if len(vanishing_points) < 2 and len(clicked_points) == 2:
             cv2.line(img, clicked_points[0], clicked_points[1], (255,0,0), 3)
             
-        if len(clicked_points) == 4:
+        if len(vanishing_points) < 2 and len(clicked_points) == 4:
             cv2.line(img, clicked_points[2], clicked_points[3], (0,255,0), 3) 
 
             line1 = getLine(clicked_points[0], clicked_points[1])
@@ -60,33 +62,28 @@ def click(event, x, y, flags, param):
             cv2.line(img, closer_point2, intersec_pos, (0,0,255), 3)
             
             vanishing_points.append(intersec_pos)
-            print('Intersection at: ' + str(intersec_pos))
             emptyClickedPoints()
         	
-        if len(vanishing_points) == 2 and len(clicked_points) == 2:
+        if len(vanishing_points) == 2 and len(clicked_points) == 4:
             line1 = getLine(vanishing_points[0], vanishing_points[1])
-            line2 = getLine(clicked_points[0], clicked_points[1])
+            line2 = getLine(clicked_points[0], clicked_points[2])
             intersec_pos = getIntersectionPosition(line1, line2)
             vanishing_points.append(intersec_pos)
-            closer_point = getCloserPoint(intersec_pos, clicked_points[0], clicked_points[1])
-            cv2.line(img, closer_point, intersec_pos, (150,75,0), 3)
 
-        if len(vanishing_points) == 3 and len(clicked_points) == 3:
-            cv2.line(img, vanishing_points[2], clicked_points[2], (150,75,0), 3)
-            emptyClickedPoints()
+            cv2.line(img, clicked_points[0], intersec_pos, (150,75,0), 3)
+            cv2.line(img, clicked_points[2], intersec_pos, (150,75,0), 3)
+            cv2.line(img, vanishing_points[2], clicked_points[1], (150,75,0), 3)
+        
+        if len(vanishing_points) == 3 and len(clicked_points) == 5:
+            bottle_height = getDistance(clicked_points[2], clicked_points[3])
+            mug_height = getDistance(clicked_points[2], clicked_points[4])
+            mug_height_cm = np.round(((mug_height / bottle_height) * 26), 2)
+            print(mug_height_cm)
 
         cv2.imshow(window_name, img)
             
 cv2.setMouseCallback(window_name, click)
 while True:
-    cv2.imshow(window_name, img)
-
-    key_press = cv2.waitKey(10)
-    if key_press == ord('q'):
-        break
-    elif key_press == ord('c'):
-        img = orig_img.copy()
-        cv2.imshow(window_name, img)
-        emptyClickedPoints()
-
+    if cv2.waitKey(10) == ord('q'):
+        break 
 cv2.destroyAllWindows()
