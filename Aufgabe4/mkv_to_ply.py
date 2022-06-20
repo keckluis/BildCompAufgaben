18512db3722571c936ddb485ca1775e9316e1294
Luis Keck <luis.keck@hs-furtwangen.de>
2022-06-20 22:18:42 +0200

    refine iterations

--- Aufgabe4/mkv_to_ply.py
+++ Aufgabe4/mkv_to_ply.py
@@ -1,19 +1,24 @@
 import os
 
 # get source file
-src = input("mkv file: ")
+source = input("mkv file: ")
 
 # get folder name
-trgt = input("result folder name: ")
+target = input("result folder name: ")
+
+# refining iterations
+iterations = input("number of refining iteration: ")
 
 path = "open3d\\examples\\reconstruction_system\\"
 
 # get color images and depth maps from mkv
-os.system(path + "sensors\\azure_kinect_mkv_reader.py --input " + src + " --output " + trgt)
+os.system(path + "sensors\\azure_kinect_mkv_reader.py --input " + source + " --output " + target)
 
 # 3D reconstruction
-rec_command = "python " + path + "run_system.py " + trgt + "\\config.json"
-os.system(rec_command + " --make")
-os.system(rec_command + " --register")
-os.system(rec_command + " --refine")
-os.system(rec_command + " --integrate")
+reconstruction_command = "python " + path + "run_system.py " + target + "\\config.json"
+os.system(reconstruction_command + " --make")
+os.system(reconstruction_command + " --register")
+
+for i in range(iterations):
+    os.system(reconstruction_command + " --refine")
+os.system(reconstruction_command + " --integrate")
